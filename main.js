cd32d1d2ad1302df9f6b214bb00a8650f61fe00f
juju2010 <78252802+juju2010@users.noreply.github.com>
2021-11-15 20:33:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,15 +11,19 @@ function setup(){
 }
 
 function draw(){
-    image(video, 90,100, 400, 300);
-    circle(50, 50, 80);
-    circle(540, 50, 80);
+    image(video, 70,70, 450, 350);
+    fill("#f5f502");
+    stroke("#f5a802");
     rect(85, 30, 420, 40);
-    circle(50, 440, 80);
-    circle(540, 440, 80);
     rect(85, 420, 420, 40);
     rect(30, 80, 40, 330);
     rect(520, 80, 40, 330);
+    fill("#b103fc");
+    stroke("#0335fc");
+    circle(50, 50, 80);
+    circle(540, 50, 80);
+    circle(50, 440, 80);
+    circle(540, 440, 80);
 }
 
 function take_snapshot(){
